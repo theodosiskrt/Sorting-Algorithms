bf795067d36a37ea3d9c0c6a1efbc69438314755
theodosiskrt <76492551+theodosiskrt@users.noreply.github.com>
2021-05-14 14:29:08 +0300

    Add files via upload

--- Sorting Algorithms/bubbleSort.js
+++ Sorting Algorithms/bubbleSort.js
@@ -1,19 +1,19 @@
 
 let found = true;
 const bubbleSort = (array) => {
-    let i = array.length
-    while (i >= 0 && found == true) {
+    for (let i = array.length; i > 0; i++) {
         found = false;
         for (let j = 0; j < i; j++)
             if (array[j] < array[j - 1]) {
                 [array[j], array[j - 1]] = [array[j - 1], array[j]];
                 found = true;
             }
+        if (!found) break;
     }
     return array;
 }
 
-const testArray = [1, 34, 5, 453, 97, 44, 2, 4352, 4, 76, 90, 124, 333];
+const testArray = [4352, 34, 5, 1, 453, 97, 44, 2, 4, 76, 90, 124, 333];
 console.log("*****************************");
 console.log("       BUBBLE SORT!");
 console.log("*****************************");
